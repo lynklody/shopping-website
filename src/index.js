1491cd99b6398b8b23c986aeab0159a6caa2ab8a
lynklody <liuai_1995@live.cn>
2020-02-23 22:05:09 +0800

    Add: logger middleware (custom middleware)

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 import thunkMiddleware from 'redux-thunk'
-import App from './component/App';
+import App from './component/App'
+import loggerMiddleware from "./middlewares/logger"
 // import * as serviceWorker from './serviceWorker';
 // import store from './store'
 // import './index.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, 
-    composeEnhancers(applyMiddleware(thunkMiddleware)));
+    composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware)));
 
 ReactDOM.render(
     <Provider store={store}>
