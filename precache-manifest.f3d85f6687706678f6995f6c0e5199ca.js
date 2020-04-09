7b00f03679e86930ffba023369bfa0359eaf1c37
lynklody <liuai_1995@live.cn>
2020-04-09 18:58:24 +0800

    Updates

---
+++ precache-manifest.f3d85f6687706678f6995f6c0e5199ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb1c82f14994bf135f7c4075a0899e5",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "f16f865da4a8d39606ae",
+    "url": "/todo-list/static/js/2.2d23ddd5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.2d23ddd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2397718160f34429d5b",
+    "url": "/todo-list/static/js/main.2f44b4d7.chunk.js"
+  },
+  {
+    "revision": "5839c7e53c96af5b08a7",
+    "url": "/todo-list/static/js/runtime-main.ab2ca7b1.js"
+  }
+]);
